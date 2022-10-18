97b109ad84608f2c0dc69965927a619b3f25e0e9
kaminaduck <charlierogers.3@gmail.com>
2022-10-17 20:14:50 -0500

    same

---
+++ cve_report.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This is a webscraper for gathering the latest CVE releases from public access.
+
+References:
+https://www.google.com
+"""
+
+# built-in module imports here
+
+# third-party module imports
+
+# path changes and personal modules
+
+# authors are the writers of the code
+__author__ = "Dade Murphy"
+__authors__ = ["Emmanuel Goldstein, Kate Libby, Dade Murphy, Paul Cook"]
+__contact__ = "info@hacktheplanet.com"
+__copyright__ = "Copyright 1994, The Ellingson Gibson Project"
+# credits go to those who offered suggestions, reported big fixes, but did not write code
+__credits__ = ["Emmanuel Goldstein", "Kate Libby", "Dade Murphy",
+                    "Paul Cook", "Joey Pardella", 
+                    "Phantom Phreak","Razor and Blade"]
+__date__ = "YYYY/MM/DD"
+__deprecated__ = False
+__email__ = "zerocool@crashoverride.com"
+__license__ = "GPL" # or proprietary
+__maintainer__ = "Dade Murphy"  # person who will fix bugs and make improvements if imported
+__status__ = "Production"   # also "Prototype" or "Development"
+__version__ = "1.0.1"   # Major.minor.patch
+
+# pre-define global variables
+
+# set values for global variables
+
+# define classes and functions
+
+# main code goes here
